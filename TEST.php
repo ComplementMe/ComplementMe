2318c408989a3920add0ddaa3d2f0e35e986a3d1
Ch3n <Ch3n@10.124.129.50>
2015-05-12 21:04:52 +0800

    Debug Heroku

--- TEST.php
+++ TEST.php
@@ -14,13 +14,13 @@ require('vendor/autoload.php');
 
 $grapheneUrl = parse_url("http://app36675546:wCMi0lmUvgUi0YZCDE4W@app36675546.sb05.stations.graphenedb.com:24789");
 
-//echo var_dump($grapheneUrl);
+echo var_dump($grapheneUrl);
 
 $client = new Everyman\Neo4j\Client($grapheneUrl['host'], $grapheneUrl['port']);
 
 $client->getTransport()->setAuth($grapheneUrl['user'], $grapheneUrl['pass']);
 
 
-echo var_dump($client);
+
 //print_r($client->getServerInfo());
 ?>
