831fde4d3bfb40b12956c3c59daac01a5dec418e
Ch3n <Ch3n@192.168.0.102>
2015-05-12 13:24:23 +0800

    Question CRUD operations Updated folder structure

---
+++ ReadNode/ReadAllQuestions.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once('../Config/DBConnection.php');
+
+//enable cross origin resources sharing (CORS)
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+}
+
+
+
+$queryString = "MATCH (n:Question) RETURN n";
+
+
+$query = new Everyman\Neo4j\Cypher\Query($client, $queryString);
+$result = $query->getResultSet();
+
+
+//return name of user. can add on properties if required
+foreach ($result as $row) {
+    echo $row['x']->getProperty('question') . "\n";
+}
+?>
