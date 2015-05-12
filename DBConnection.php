0f458dc20cc7aed635e6e1eba8203abd358e9b0d
Ch3n <Ch3n@10.124.129.50>
2015-05-12 19:59:12 +0800

    Debug Heroku

--- DBConnection.php
+++ DBConnection.php
@@ -11,11 +11,10 @@
 require_once('../vendor/autoload.php');
 
 // ...
-//$grapheneUrl = parse_url("http://app36675546:wCMi0lmUvgUi0YZCDE4W@app36675546.sb05.stations.graphenedb.com:24789");
+$grapheneUrl = parse_url("http://app36675546:wCMi0lmUvgUi0YZCDE4W@app36675546.sb05.stations.graphenedb.com:24789");
 //heroku environment variable
-
-echo getenv('GRAPHENEDB_URL');
-$grapheneUrl = parse_url(getenv('GRAPHENEDB_URL'));
+//echo getenv('GRAPHENEDB_URL');
+//$grapheneUrl = parse_url(getenv('GRAPHENEDB_URL'));
 
 
 $client = new Everyman\Neo4j\Client($grapheneUrl['host'], $grapheneUrl['port']);
