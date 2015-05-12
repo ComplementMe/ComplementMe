50fab805a0be0a69ceb577c1cc673dba591614aa
Ch3n <Ch3n@192.168.0.102>
2015-05-12 13:29:11 +0800

    Answer Node CRUD operations

---
+++ DeleteNode/DeleteAnswer.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once('../Config/DBConnection.php');
+
+//enable cross origin resources sharing (CORS)
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+}
+
+
+
+$getAnswer = htmlspecialchars($_GET['answer']);
+
+
+
+//delete node with relations
+
+$queryString = "MATCH (n:Answer { question: '" . $getAnswer . "' })-[r]-() DELETE n,r";
+$query = new Everyman\Neo4j\Cypher\Query($client, $queryString);
+$result = $query->getResultSet();
+
+
+//delete node with no relations
+$queryString = "MATCH (n:Answer { question: '" . $getAnswer . "' }) DELETE n";
+$query = new Everyman\Neo4j\Cypher\Query($client, $queryString);
+$result = $query->getResultSet();
+?>
