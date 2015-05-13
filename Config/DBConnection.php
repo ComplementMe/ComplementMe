b8151f4345608a617eb00f5458c07c10090ca569
Ch3n <Ch3n@192.168.0.102>
2015-05-13 23:55:31 +0800

    Debug Heroku

--- Config/DBConnection.php
+++ Config/DBConnection.php
@@ -1,26 +1,24 @@
 <?php
 
-// https://github.com/jadell/neo4jphp
-// in composer.json:
-// {
-//   "require": {
-//     "everyman/neo4jphp": "dev-master"
-//   }
-// }
-// require at the top of the script
-require_once('../vendor/autoload.php');
+require_once '../vendor/autoload.php';
+
+use Neoxygen\NeoClient\ClientBuilder;
 
 // ...
-$grapheneUrl = parse_url("http://app36675546:wCMi0lmUvgUi0YZCDE4W@app36675546.sb05.stations.graphenedb.com:24789");
-//heroku environment variable
-//echo getenv('GRAPHENEDB_URL');
-//$grapheneUrl = parse_url(getenv('GRAPHENEDB_URL'));
+
+$url = parse_url("http://app36675546:wCMi0lmUvgUi0YZCDE4W@app36675546.sb05.stations.graphenedb.com:24789");
+
+////this line is the problem with heroku... it cant seem to detect the class.
+//$client = new Everyman\Neo4j\Client($grapheneUrl['host'], $grapheneUrl['port']);
+//echo var_dump($client);
+//
+//$client->getTransport()->setAuth($grapheneUrl['user'], $grapheneUrl['pass']);
+//
+$client = ClientBuilder::create()
+        ->addConnection('default', $url['scheme'], $url['host'], $url['port'], true, $url['user'], $url['pass'])
+        ->setAutoFormatResponse(true)
+        ->build();
 
 
-$client = new Everyman\Neo4j\Client($grapheneUrl['host'], $grapheneUrl['port']);
-
-$client->getTransport()->setAuth($grapheneUrl['user'], $grapheneUrl['pass']);
-
-// Test connection to server
-//print_r($client->getServerInfo());
+print_r($client->getNeoClientVersion());
 ?>
