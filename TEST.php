2b871289d59a0ea27d546a15d1f0e9ba33230449
Ch3n <Ch3n@192.168.0.102>
2015-05-13 23:23:39 +0800

    Debug Heroku

--- TEST.php
+++ TEST.php
@@ -8,19 +8,26 @@
 //   }
 // }
 // require at the top of the script
-require('vendor/autoload.php');
+//require('vendor/autoload.php');
+
+require_once 'vendor/autoload.php';
+
+use Neoxygen\NeoClient\ClientBuilder;
 
 // ...
 
 $grapheneUrl = parse_url(getenv('GRAPHENEDB_URL'));
 
-//this line is the problem with heroku... it cant seem to detect the class.
-$client = new Everyman\Neo4j\Client($grapheneUrl['host'], $grapheneUrl['port']);
-echo var_dump($client);
-
-$client->getTransport()->setAuth($grapheneUrl['user'], $grapheneUrl['pass']);
-
-
+////this line is the problem with heroku... it cant seem to detect the class.
+//$client = new Everyman\Neo4j\Client($grapheneUrl['host'], $grapheneUrl['port']);
+//echo var_dump($client);
+//
+//$client->getTransport()->setAuth($grapheneUrl['user'], $grapheneUrl['pass']);
+//
+$client = ClientBuilder::create()
+        ->addConnection('default', $url['scheme'], $url['host'], $url['port'], true, $url['user'], $url['pass'])
+        ->setAutoFormatResponse(true)
+        ->build();
 
 //print_r($client->getServerInfo());
 ?>
